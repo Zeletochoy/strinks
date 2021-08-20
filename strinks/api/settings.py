f9800e7a56d78d4293b6a1fc6e0d280771e07a01
Antoine Lecubin <alecubin@cogent.co.jp>
2021-08-20 10:58:59 +0900

    Get settings from env

---
+++ strinks/api/settings.py
@@ -0,0 +1,12 @@
+import sys
+from os import environ
+
+
+try:
+    DEEPL_API_KEY = environ["DEEPL_API_KEY"]
+
+    UNTAPPD_CLIENT_ID = environ["UNTAPPD_CLIENT_ID"]
+    UNTAPPD_CLIENT_SECRET = environ["UNTAPPD_CLIENT_SECRET"]
+except KeyError as e:
+    print(f"Missing value for {e} in environment")
+    sys.exit(1)
