b59faa962e0f8de3adace68b3369ff8d9f2ae600
Antoine Lecubin <alecubin@cogent.co.jp>
2022-04-27 01:31:16 +0900

    Better title matching for volta

--- strinks/api/shops/volta.py
+++ strinks/api/shops/volta.py
@@ -1,13 +1,27 @@
 import re
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, Optional
+from urllib.parse import urlparse
+from pathlib import Path
 
-import requests
 from bs4 import BeautifulSoup
 from unidecode import unidecode
 
 from ...db.models import BeerDB
 from ...db.tables import Shop as DBShop
+from ..utils import get_retrying_session
 from . import NoBeersError, NotABeerError, Shop, ShopBeer
+
+
+session = get_retrying_session()
+
+
+def _extract_untappd_beer_name(link_url: str) -> Optional[str]:
+    if "untappd.com" not in link_url:
+        return None
+    path = Path(urlparse(link_url).path)
+    if path.parts[:2] != ["/", "b"] or len(path.parts) < 3 :
+        return None
+    return path.parts[2].replace("-", " ")
 
 
 class Volta(Shop):
@@ -18,33 +32,48 @@ class Volta(Shop):
         i = 1
         while True:
             url = f"http://beervolta.com/?mode=srh&sort=n&cid=&keyword=&page={i}"
-            page = requests.get(url).text
+            page = session.get(url).text
             yield BeautifulSoup(page, "html.parser")
             i += 1
 
     def _iter_page_beers(self, page_soup: BeautifulSoup) -> Iterator[Tuple[BeautifulSoup, str]]:
         empty = True
+        if (recently_viewed := page_soup.find("div", id="box_recentlychecked")) is not None:
+            recently_viewed.decompose()
         for item in page_soup("div", class_="item_box"):
+            if item.find("span", class_="sold_out") is not None:
+                continue
             url = "http://beervolta.com/" + item.find("a")["href"]
-            page = requests.get(url).text
+            page = session.get(url).text
             yield BeautifulSoup(page, "html.parser"), url
             empty = False
         if empty:
             raise NoBeersError
 
     def _parse_beer_page(self, page_soup, url) -> ShopBeer:
-        if page_soup.find("div", class_="sold_out") is not None:
-            raise NotABeerError
-        footstamp = page_soup.find("div", class_="footstamp")
-        title = page_soup.find("h1", class_="product_name").get_text().strip()
-        title = re.sub(r" \d\d?/\d\d?入荷予定", "", title)
-        if "　" in title:
-            raw_name = title.rsplit("　", 1)[-1]
-        else:
-            brewery_p = footstamp("p")[-1]
-            brewery = unidecode(brewery_p("a")[-1].get_text().strip())
-            raw_name = brewery + unidecode(title).rsplit(brewery, 1)[-1]
-        raw_name = raw_name.replace("\t", " ").replace("  ", " ").lower()
+        raw_name = None
+        desc = page_soup.find("div", class_="product_description")
+        if desc is not None:
+            for link in desc("a"):
+                if raw_name := _extract_untappd_beer_name(link.get("href", "")):
+                    break
+        if raw_name is None:
+            footstamp = page_soup.find("div", class_="footstamp")
+            title = page_soup.find("h1", class_="product_name").get_text().strip()
+            title = re.sub(r" \d\d?/\d\d?入荷予定", "", title)
+            title = re.sub(r" *\[[^]]+\] *", "", title)
+            on_sale = any(link.get_text().strip() == "セール" for link in page_soup("a"))
+            if on_sale:
+                title = re.sub(r"^[^!]*!+ *", "", title)  # remove prefix like "SALE!!"
+            if "　" in title:
+                raw_name = title.rsplit("　", 1)[-1]
+            elif "/" in title:
+                raw_name = title.rsplit("/", 1)[-1]
+            else:
+                brewery_p = footstamp("p")[-1]
+                brewery = unidecode(brewery_p("a")[-1].get_text().strip())
+                raw_name = brewery + unidecode(title).rsplit(brewery, 1)[-1]
+            raw_name = raw_name.replace("\t", " ").replace("  ", " ").lower()
         cart_table = page_soup.find("table", class_="add_cart_table")
         for row in cart_table("tr"):
             try:
