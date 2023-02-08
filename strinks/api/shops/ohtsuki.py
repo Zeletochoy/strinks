d1c7332b5b1685ec4c4f9f4dbbc7667e2fc70110
Antoine Lecubin <alecubin@cogent.co.jp>
2023-02-08 17:10:25 +0900

    Fix Ohtsuki

--- strinks/api/shops/ohtsuki.py
+++ strinks/api/shops/ohtsuki.py
@@ -17,35 +17,33 @@ class Ohtsuki(Shop):
     display_name = "Ohtsuki"
 
     def iter_beers(self) -> Iterator[ShopBeer]:
-        base_url = "http://www.ohtsuki-saketen.com/catalog/"
-        page = session.get(base_url).content.decode("sjis")
-        page_soup = BeautifulSoup(page, "html.parser")
-        for row in page_soup("tr"):
-            try:
+        base_url = "https://ohtsuki-saketen.com/beer/index.html"
+        page_soup = BeautifulSoup(session.get(base_url).text, "html.parser")
+        for table in page_soup("table", class_="product"):
+            for row in page_soup("tr"):
                 try:
-                    name_cell, ml_cell, price_cell, avail_cell = row("td")
-                except ValueError:
-                    continue
-                if avail_cell.get_text().strip() == "X":
-                    continue  # Sold Out
-                url = base_url + name_cell.find("a")["href"]
-                page = session.get(url).text.replace("<br/>", "\n")
-                beer_soup = BeautifulSoup(page, "html.parser")
-                image_url = base_url + beer_soup.find("div", class_="img").find("img")["src"]
-                raw_name = name_cell.get_text("\n").lower().split("\n", 1)[0]
-                raw_name = re.sub("( ?(大瓶|初期|Magnum|Jeroboam|alc[.].*))*$", "", raw_name)
-                ml = int(ml_cell.get_text().strip().replace("ml", ""))
-                price = int(int(price_cell.get_text().strip().replace("円", "")) * 1.1)  # tax
-                yield ShopBeer(
-                    raw_name=raw_name,
-                    url=url,
-                    milliliters=ml,
-                    price=price,
-                    quantity=1,
-                    image_url=image_url,
-                )
-            except Exception as e:
-                print(f"Unexpected exception while parsing page, skipping.\n{e}")
+                    try:
+                        name_cell, _, ml_cell, price_cell, avail_cell = row("td")
+                    except ValueError:
+                        continue
+                    if avail_cell.get_text().strip() == "X":
+                        continue  # Sold Out
+                    url = base_url + name_cell.find("a")["href"]
+                    image_url = base_url.replace(".html", ".jpg")
+                    raw_name = name_cell.get_text("\n").lower().split("\n", 1)[0]
+                    raw_name = re.sub("( ?(大瓶|初期|Magnum|Jeroboam|alc[.].*))*$", "", raw_name)
+                    ml = int(ml_cell.get_text().strip().replace("ml", ""))
+                    price = int(price_cell.get_text().strip().replace("円", "").replace(",", ""))
+                    yield ShopBeer(
+                        raw_name=raw_name,
+                        url=url,
+                        milliliters=ml,
+                        price=price,
+                        quantity=1,
+                        image_url=image_url,
+                    )
+                except Exception as e:
+                    print(f"Unexpected exception while parsing page, skipping.\n{e}")
 
     def get_db_entry(self, db: BeerDB) -> DBShop:
         return db.insert_shop(
