5b2101b62dcdcc089c88ffadaeb21e48a5465a18
Antoine Lecubin <a-lecubin@mercari.com>
2023-10-20 16:19:27 +0900

    Update ibrew scraper

--- strinks/api/shops/ibrew.py
+++ strinks/api/shops/ibrew.py
@@ -20,16 +20,13 @@ class IBrew(Shop):
             day = date.today()
         self.day = day
         self.prices = {}
+        self.html_url = f"https://menu.craftbeerbar-ibrew.com/{self.location}-menu/todays-beer/"
         self._set_urls()
 
     def _set_urls(self) -> None:
         self.json_url = (
-            f"https://craftbeerbar-ibrew.com/{self.location}-menu/wp-json/beer/v1/"
+            f"https://menu.craftbeerbar-ibrew.com/{self.location}-menu/wp-json/beer/v1/"
             f"graded_taps/{self.day.year}/{self.day.month}/{self.day.day}/"
-        )
-        self.html_url = (
-            f"https://craftbeerbar-ibrew.com/{self.location}-menu/beermenu/"
-            f"{self.day.year}年{self.day.month}月{self.day.day}日-ビールメニュー/"
         )
 
     def _get_grade_price(self, grade: str) -> int:
@@ -38,14 +35,21 @@ class IBrew(Shop):
     def _set_grade_prices(self, api_json: dict) -> None:
         self.prices = {name[0]: grade["pint"] for name, grade in api_json["prices"].items() if isinstance(grade, dict)}
 
+    def _compute_price(self, tap_json: dict) -> int:
+        price = self._get_grade_price(tap_json["grade"])
+        for special_block in tap_json.get("special_price", "").split(","):
+            if not special_block:
+                continue
+            type, extra = special_block.split(":")
+            if type == "p":
+                price += int(extra)
+        return price
+
     def _parse_beer(self, tap_json: dict) -> Iterator[ShopBeer]:
         brewery_name = tap_json.get("brewer")
         beer_name = tap_json.get("beer")
         image_url = tap_json.get("logo_url")
-        if tap_json.get("special_price"):
-            price = int(tap_json["special_price"])
-        else:
-            price = self._get_grade_price(tap_json["grade"])
+        price = self._compute_price(tap_json)
         yield ShopBeer(
             raw_name=f"{brewery_name} {beer_name}",
             url=self.html_url,
