a9ac48f70341a6f18be6a44ed53f3856ada3ce74
Antoine Lecubin <a-lecubin@mercari.com>
2024-05-17 17:06:56 +0900

    Fix Antenna scraper

--- strinks/api/shops/antenna.py
+++ strinks/api/shops/antenna.py
@@ -1,6 +1,6 @@
 import re
-import time
 from typing import Iterator
+from urllib.parse import urlparse, urlunparse
 
 from bs4 import BeautifulSoup
 
@@ -15,6 +15,12 @@ DIGITS = set("0123456789")
 session = get_retrying_session()
 
 
+def _get_json_url(beer_url: str) -> str:
+    parts = list(urlparse(beer_url))
+    parts[2] = parts[2] + ".oembed"  # Add .oembed to path
+    return urlunparse(parts)
+
+
 class AntennaAmerica(Shop):
     short_name = "antenna"
     display_name = "Antenna America"
@@ -22,76 +28,52 @@ class AntennaAmerica(Shop):
     def _iter_pages(self) -> Iterator[BeautifulSoup]:
         i = 1
         while True:
-            ts = int(time.time() * 1000)
-            url = (
-                f"https://services.mybcapps.com/bc-sf-filter/filter?t={ts}&sort=created-descending&_=pf"
-                f"&shop=antenna-america-shop.myshopify.com&page={i}&limit=32&display=grid"
-                "&collection_scope=226404991137&tag=&product_available=true&variant_available=true"
-                "&build_filter_tree=true&check_cache=false&sort_first=available&locale=ja&event_type=history"
-            )
-            yield session.get(url).json()
+            url = f"https://www.antenna-america.com/collections/beer?page={i}&sort_by=created-descending"
+            page = session.get(url).text
+            yield BeautifulSoup(page, "html.parser")
             i += 1
 
-    def _iter_page_beers(self, page_json: dict) -> Iterator[dict]:
+    def _iter_page_beers(self, page_soup: BeautifulSoup) -> Iterator[dict]:
         empty = True
-        for item in page_json["products"]:
-            yield item
+        for item_li in page_soup("li", class_="grid__item"):
+            url = "https://www.antenna-america.com" + item_li.find("a")["href"]
+            url = _get_json_url(url)
+            yield session.get(url).json()
             empty = False
         if empty:
             raise NoBeersError
 
-    def _parse_beer_page(self, beer_item: dict) -> ShopBeer:
-        brewery_name = beer_item["product_type"].lower()
-        if not brewery_name:
+    def _parse_beer_page(self, page_json: dict) -> ShopBeer:
+        raw_name = re.split(r"\([0-9０-９]+(ml|ｍｌ)\)", page_json["title"].lower())[0].strip()
+        raw_name = re.sub("【[^】]*】", "", raw_name)
+        if "本セット" in raw_name:
             raise NotABeerError
-        title = beer_item["title"].lower()
-        title = re.sub("【[^】]*】", "", title)
-        title = title[len(brewery_name) :]
-        beer_name = title.split("/", 1)[0].strip()
-        if "pack" in beer_item.get("vendor", "").lower():
-            raise NotABeerError
-        price = beer_item["price_min"]
-        image_url = beer_item["images_info"][0]["src"]
-        url = f"https://www.antenna-america.com/en/collections/all/products/{beer_item['handle']}"
-        match = re.search(r"\((\d+)ml\)", beer_name)
+        price = int(page_json["offers"][0]["price"])
+        image_url = "https:" + page_json["thumbnail_url"]
+        url = page_json["url"]
+        desc = page_json["description"]
+        match = re.search(r"([0-9０-９]+)(ml|ｍｌ)", desc.lower())
         if match is not None:
             ml = int(match.group(1))
-            beer_name = beer_name[: -len(match.group(0)) - 1]
+        match = re.search(r"ブリュワリー：([^<]+)<", desc.lower())
+        if match is not None:
+            brewery_name = match.group(1)
+            beer_name = raw_name[len(brewery_name) + 1:]
         else:
-            page = session.get(url).text
-            soup = BeautifulSoup(page, features="html.parser")
-            table = soup.find(id="PartsItemAttribute")
-            if table is not None:
-                for row in table("tr"):
-                    try:
-                        row_name = row.find("th").get_text().strip()
-                        row_value = row.find("td").get_text().strip()
-                    except AttributeError:
-                        continue
-                    if row_name == "内容量":
-                        try:
-                            ml = int("".join(c for c in row_value if c in DIGITS))
-                            break
-                        except ValueError:
-                            raise NotABeerError
-                else:
-                    raise NotABeerError
-            else:
-                desc = soup.find("div", class_="product-single__description").get_text().lower()
-                ml_match = re.search(r"(\d{3,4})ml", desc)
-                if ml_match is None:
-                    raise NotABeerError
-                ml = int(ml_match.group(1))
-        return ShopBeer(
-            beer_name=beer_name,
-            brewery_name=brewery_name,
-            raw_name=brewery_name + " " + beer_name,
-            url=url,
-            milliliters=ml,
-            price=price,
-            quantity=1,
-            image_url=image_url,
-        )
+            brewery_name = beer_name = None
+        try:
+            return ShopBeer(
+                raw_name=raw_name,
+                brewery_name=brewery_name,
+                beer_name=beer_name,
+                url=url,
+                milliliters=ml,
+                price=price,
+                quantity=1,
+                image_url=image_url,
+            )
+        except UnboundLocalError:
+            raise NotABeerError
 
     def iter_beers(self) -> Iterator[ShopBeer]:
         for listing_page in self._iter_pages():
